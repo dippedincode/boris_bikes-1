2c32998c206bde8386f5bbfb1822fa06249856c0
david-mears <kieukmail@gmail.com>
2019-01-09 16:34:25 +0000

    Added test to dock bike and the method to pass the test

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,20 @@
 class DockingStation
+  attr_reader :is_full
+
+  def initialize
+    @is_full = false
+  end
+
   def release_bike
     Bike.new
+  end
+
+  def dock_bike
+    @is_full = true
+    return @is_full
+  end
+
+  def full?
+    return is_full
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,14 +2,20 @@ require 'docking_station'
 
 describe DockingStation do
 #  subject { DockingStation.new }
-  it { is_expected.to respond_to(:release_bike)}
+  it { is_expected.to respond_to :release_bike}
 
   it "gets a bike" do
-    expect(Bike.new.working?).to be true
+    expect(Bike.new).to be_working
   end
 
-end
+  it { is_expected.to respond_to :dock_bike }
 
-describe DockingStation.new.release_bike do
-  it { is_expected.to be }
+  it "takes a bike" do
+    p "test:"
+    p DockingStation.new.dock_bike
+    stn = DockingStation.new
+    stn.dock_bike
+    expect(stn).to be_full
+  end
+
 end
