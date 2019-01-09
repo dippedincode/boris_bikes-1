118be3706b15fc22f5a92463ff1db901c16e3d05
david-mears <kieukmail@gmail.com>
2019-01-09 17:31:26 +0000

    More tests on release_bike (passed)

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,13 +4,26 @@ describe DockingStation do
 #  subject { DockingStation.new }
   it { is_expected.to respond_to :release_bike}
 
+  it "releases a bike" do
+    stn = DockingStation.new
+    stn.bike_arr.push(Bike.new)
+    expect(stn.release_bike).to be_a Bike
+  end
+
+  it "releases a bike and drops it from the array" do
+    stn = DockingStation.new
+    19.times { stn.bike_arr.push(Bike.new) }
+    10.times { stn.release_bike }
+    expect(stn.bike_arr.count).to be 9
+  end
+
   it "tells if bike is working" do
     expect(Bike.new).to be_working
   end
 
   it { is_expected.to respond_to :dock_bike }
 
-  it "takes a bike" do
+  it "takes a bike and puts it in the array" do
     stn = DockingStation.new
     before = stn.bike_arr.count
     stn.dock_bike(Bike.new)
